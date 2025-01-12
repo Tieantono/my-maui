06680ac93169acc6f5cc40d8c88c92a1a36c325e
Tie Antono <tietieantono@gmail.com>
2025-01-12 23:15:36 +0700

    Added Z.Expressions.Eval package

--- MyMaui/MainPage.xaml.cs
+++ MyMaui/MainPage.xaml.cs
@@ -1,25 +1,85 @@
-﻿namespace MyMaui
+﻿using MyMaui.Models;
+using Z.Expressions;
+
+namespace MyMaui;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
+        EvaluateRequest();
     }
 
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private void EvaluateRequest()
+    {
+        var list = new List<int>() { 1, 2, 3, 4 };
+        var greaterThan = 2;
+
+        list = Eval.Execute<List<int>>("list.Where(x => x > greaterThan)",
+           new { list, greaterThan });
+
+        var exp = @"(Convert.ToDateTime(TransDate) >= Convert.ToDateTime(""2024-10-21 07:26:00"")) 
+        && (Convert.ToDateTime(""2025-10-21 07:24:00"") >= Convert.ToDateTime(TransDate)) 
+        && (Convert.ToDecimal(ItemProduct
+            .Sum(y => Convert.ToDecimal(y.Price) * Convert.ToDecimal(y.Qty))) >= Convert.ToDecimal(""100000"")) 
+            && (ZoneCode == ""Zone1"" ) 
+            && (SiteCode == ""Site1"" ) 
+        && (PromoAppCode == ""Promo1"") 
+        && ((Mop.FirstOrDefault(q => q.MopCode == ""Mop1"").Amount) >= 100000) ";
+
+        var req = new PromoRequest
+        {
+            TransDate = "2025-01-02 12:00:00",
+            ZoneCode = "Zone1",
+            SiteCode = "Site1",
+            PromoAppCode = "Promo1",
+            ItemProduct = [
+                new ItemProduct{
+                    Price = 50_000,
+                    Qty = 1
+                },
+                new ItemProduct{
+                    Price = 50_000,
+                    Qty = 2
+                }
+            ],
+            Mop = [
+                new Mop {
+                   Amount = 100_000,
+                   MopCode = "Mop1"
+                }
+            ]
+        };
+
+        EvalContext context = new();
+        var isValid = context.Execute<bool>(exp, req);
+
+        var itemList = list.Select(Q => Q.ToString()).ToList();
+
+        if (isValid)
+        {
+            TestLabel.Text = "Eval is true";
+            itemList.Add(TestLabel.Text);
+        }
+        else
+        {
+            TestLabel.Text = "Eval is false";
+            itemList.Add(TestLabel.Text);
+        }
+    }
 }
---
+++ MyMaui/Models/PromoRequest.cs
@@ -0,0 +1,30 @@
+﻿namespace MyMaui.Models;
+
+public class PromoRequest
+{
+    public string PromoAppCode { get; set; } = string.Empty;
+
+    public string TransDate { get; set; } = string.Empty;
+
+    public string ZoneCode { get; set; } = string.Empty;
+
+    public string SiteCode { get; set; } = string.Empty;
+
+    public List<ItemProduct> ItemProduct { get; set; } = [];
+
+    public List<Mop> Mop { get; set; } = [];
+}
+
+public class ItemProduct
+{
+    public int Price { get; set; }
+
+    public int Qty { get; set; }
+}
+
+public class Mop
+{
+    public string MopCode { get; set; } = string.Empty;
+
+    public int Amount { get; set; }
+}
